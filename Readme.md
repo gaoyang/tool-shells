b9724fb8e637f150614213df5de5fc427d724b10
gaoyang <xz@lgysh.cn>
2022-09-05 22:44:03 +0800

    add git-edit-commit shell

--- Readme.md
+++ Readme.md
@@ -1,10 +1,19 @@
-TOOL_SHELLS
-====
+# TOOL_SHELLS
+
 个人常用脚本工具集合。
 
-nuget-clean
-----------
+## nuget-clean
+
 Platform: mac/win/linux
+
 ```Shell
-nuget-clean.sh 
+nuget-clean.sh
+```
+
+## git-edit-commit
+
+Platform: mac/win/linux
+
+```Shell
+git-edit-commit.sh [repositoryPath]
 ```
---
+++ git-edit-commit.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+cd $1
+
+read -r -p "输入要修改的邮箱地址:" email
+read -r -p "新的邮箱:" new_email
+read -r -p "新的名称:" new_name
+
+git filter-branch -f --env-filter '
+if [ "$GIT_COMMITTER_EMAIL" = "'${email}'" ]
+then
+    export GIT_COMMITTER_NAME="'${new_name}'"
+    export GIT_COMMITTER_EMAIL="'${new_email}'"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "'${email}'" ]
+then
+    export GIT_AUTHOR_NAME="'${new_name}'"
+    export GIT_AUTHOR_EMAIL="'${new_email}'"
+fi
+' --tag-name-filter cat -- --branches --tags
+
+read -r -p "是否强制推送至服务器？(Y/n)" response
+
+if [[ "$response" =~ ^([yY])$ ]]; then
+    git push --force --tags origin 'refs/heads/*'
+fi
